389ff90ea1de861d7ca64f343d18c89b2f3c5c1e
Ninda <hirianinda.malsegianty.s97@gmail.com>
2022-08-20 22:17:10 +0700

    ubah nama sidebar

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -48,14 +48,14 @@
     @hasrole('Admin')
     <!-- Heading -->
     <div class="sidebar-heading">
-        Admin Section
+        Alert Section
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
             <i class="fas fa-fw fa-folder"></i>
-            <span>Masters</span>
+            <span>Alerts</span>
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
