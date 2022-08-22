4aa29c4d1f45b2c8a3599b75011f3c70d9d02ef0
Ninda <hirianinda.malsegianty.s97@gmail.com>
2022-08-22 09:20:13 +0700

    update names

--- resources/views/data-alert.blade.php
+++ resources/views/data-alert.blade.php
@@ -9,7 +9,7 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Data Alert Telegram</h1>
-        <a href="{{route('users.index')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-arrow-left fa-sm text-white-50"></i> Back</a>
+        <a href="{{route('home')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-arrow-left fa-sm text-white-50"></i> Back</a>
     </div>
 
     {{-- Alert Messages --}}
