02407f2fd0e10479540fb40ac8d79ef79302a634
Ninda <hirianinda.malsegianty.s97@gmail.com>
2022-08-23 11:40:18 +0700

    update chat_id -> chatid

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'chat_id',
+        'chatid',
         'role_id',
         'password',
     ];
