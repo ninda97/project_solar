40ae44b8e356c549c5be171f67638a0442a3075d
Aldy Kurnaiwan S <aldy.kurniawan17@gmail.com>
2022-09-05 14:34:25 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
     @foreach($result as $result)
         <!-- Total Alert -->
         <div class="col-xl-4 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card border-left-primary shadow h-500 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
@@ -31,7 +31,7 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{$result->totalalert}}</div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-exclamation-triangle fa-2x text-gray-300"></i>
+                            <i class="fa fa-telegram fa-2x text-gray-400"></i>
                         </div>
                     </div>
                 </div>
@@ -85,7 +85,7 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{$result->totaldown}}</div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-exclamation-triangle fa-2x text-gray-300"></i>
+                            <i class="fas fa-power-off fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
